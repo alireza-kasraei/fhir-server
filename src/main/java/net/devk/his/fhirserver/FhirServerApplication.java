dd45d2f52cfad56809a5c3c8c666bec7bd3d2174
ali <kasraei.ar@gmail.com>
2022-01-13 01:48:53 +0100

    initialize project

---
+++ src/main/java/net/devk/his/fhirserver/FhirServerApplication.java
@@ -0,0 +1,13 @@
+package net.devk.his.fhirserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/devk/his/fhirserver/FhirServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.devk.his.fhirserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
